d5d65d22673e2b78bab693fca77cd2313a10f7f4
cpodda23 <120722457+cpodda23@users.noreply.github.com>
2023-03-10 17:59:03 +0100

    getOffset

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const startDate = '2023-06-01T13:00:00Z';
 //offset sono i giorni saltati = non lavorativi
-function getOffset({ startDate, duration, workingSaturday, workingSunday }) {}
+function getOffset({ startDate, duration, workingSaturday, workingSunday }) {
+  const offset = new Date(startDate).getDate() + duration - 1;
+  console.log(offset);
+  workingSaturday && workingSunday;
+  const sat = workingSaturday ? offset : offset + 1;
+  const sun = workingSunday ? offset : offset + 1;
+  return sat;
+}
 
 console.log(
   getOffset({
